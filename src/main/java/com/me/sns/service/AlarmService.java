510626ceaf73792d57a23404626d1a3910ddae41
yessm621 <yessm621@gmail.com>
2023-07-30 15:56:44 +0900

    SseEmitter에 대한 주석 작성

--- src/main/java/com/me/sns/service/AlarmService.java
+++ src/main/java/com/me/sns/service/AlarmService.java
@@ -33,10 +33,15 @@ public class AlarmService {
     public SseEmitter connectAlarm(Integer userId) {
         SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
         emitterRepository.save(userId, sseEmitter);
+
+        // 시간 초과, 네트워크 오류를 포함한 모든 이유로 비동기 요청이 정상 동작할 수 없을 때 저장해둔 SseEmitter를 삭제함.
         sseEmitter.onCompletion(() -> emitterRepository.delete(userId));
         sseEmitter.onTimeout(() -> emitterRepository.delete(userId));
 
         try {
+            // 연결 요청에 의해 SseEmitter가 생성되면 더미 데이터를 보내줘야함.
+            // 연결된 후 하나의 데이터도 전송되지 않는다면 SseEmitter의 유효시간이 끝났을 경우
+            // 503 응답이 발생하므로 연결시 바로 더미 데이터를 한 번 보내준다.
             sseEmitter.send(SseEmitter.event().id("id").name(ALARM_NAME).data("connect complete"));
         } catch (IOException exception) {
             throw new SnsApplicationException(ErrorCode.ALARM_CONNECT_ERROR);
